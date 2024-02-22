0e40cde0760b26c16002152aee27172729e56875
yukionishi1129 <yuki.onishi1129@gmail.com>
2024-02-22 23:01:02 +0900

    practice grpc server

--- micro-service/user-service/server.go
+++ micro-service/user-service/server.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
 
-func main() {	
-	fmt.Println("Hello, World!")
+	hellopb "github.com/YukiOnishi1129/auto-stock-trading-system/pkg/grpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type myServer struct {
+	hellopb.UnimplementedGreetingServiceServer
+}
+
+func NewMyServer() *myServer {
+	return &myServer{}
+}
+
+func main() {
+	// crate a listener on TCP port 8080
+	port := 8080
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		return
+	}
+
+	// create a new gRPC server
+	s := grpc.NewServer()
+
+	// register the greeting service with the gRPC server
+	hellopb.RegisterGreetingServiceServer(s, NewMyServer())
+
+	// register reflection service on gRPC server
+	reflection.Register(s)
+
+	// start the gRPC server port at 8080
+	go func() {
+		log.Printf("start gRPC server listening on port: %d", port)
+		s.Serve(listener)
+	}()
+
+	// wait for Control C to exit
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutting down gRPC server...")
+	s.GracefulStop()
+}
+
+func (s *myServer) Hello(ctx context.Context, req *hellopb.HelloRequest) (*hellopb.HelloResponse, error) {
+	return &hellopb.HelloResponse{Message: fmt.Sprintf("Hello, %s!", req.GetName())}, nil
 }
