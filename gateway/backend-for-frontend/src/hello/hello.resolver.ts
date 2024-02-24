0e7a03b806a4f4334bd16e248be1a443edbc1db2
yukionishi1129 <yuki.onishi1129@gmail.com>
2024-02-24 23:55:45 +0900

    contact bff to user service

--- gateway/backend-for-frontend/src/hello/hello.resolver.ts
+++ gateway/backend-for-frontend/src/hello/hello.resolver.ts
@@ -1,13 +1,14 @@
 import { Resolver, Query, Args } from '@nestjs/graphql';
 import { HelloService } from './hello.service';
 import { Hello } from '../graphql/types/graphql';
-
+import { Observable } from 'rxjs';
+import { HelloResponse } from '../grpc/hello';
 @Resolver('Hello')
 export class HelloResolver {
   constructor(private readonly helloService: HelloService) {}
 
   @Query(() => Hello)
-  async hello(@Args('name') name: string): Promise<Hello> {
+  hello(@Args('name') name: string): Observable<HelloResponse> {
     return this.helloService.getHello(name);
   }
 }
--- gateway/backend-for-frontend/src/hello/hello.service.ts
+++ gateway/backend-for-frontend/src/hello/hello.service.ts
@@ -1,11 +1,32 @@
-import { Injectable } from '@nestjs/common';
-import { Hello } from '../graphql/types/graphql';
+import { Injectable, OnModuleInit, Inject } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+// import { Hello } from '../graphql/types/graphql';
+import {
+  GreetingServiceClient,
+  HelloRequest,
+  HelloResponse,
+} from '../grpc/hello';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class HelloService {
-  async getHello(name: string): Promise<Hello> {
-    return {
-      message: `This action returns a #${name} hello`,
-    };
+export class HelloService implements OnModuleInit {
+  private greetingService: GreetingServiceClient;
+
+  constructor(@Inject('HELLO_PACKAGE') private client: ClientGrpc) {}
+
+  onModuleInit() {
+    this.greetingService =
+      this.client.getService<GreetingServiceClient>('GreetingService');
+  }
+
+  getHello(name: string): Observable<HelloResponse> {
+    return this.greetingService.hello({ name } as HelloRequest);
+    // const massageList: Array<string> = [];
+    // response.forEach((res) => {
+    //   massageList.push(res.message);
+    // });
+    // return {
+    //   message: massageList[0],
+    // };
   }
 }
