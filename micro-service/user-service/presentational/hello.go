9ae2fb08f298138c995924f97d1110a6997b1b8b
yukionishi1129 <yuki.onishi1129@gmail.com>
2024-03-02 17:47:48 +0900

    remove hello gprc

---
+++ micro-service/user-service/presentational/hello.go
@@ -0,0 +1,78 @@
+package presentational
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	hellopb "github.com/YukiOnishi1129/auto-stock-trading-system/user-service/grpc"
+	"io"
+	"time"
+)
+
+type helloServer struct {
+	hellopb.UnimplementedGreetingServiceServer
+}
+
+func NewHelloServer() *helloServer {
+	return &helloServer{}
+}
+
+// Hello is a simple unary RPC
+func (s *helloServer) Hello(ctx context.Context, req *hellopb.HelloRequest) (*hellopb.HelloResponse, error) {
+	// get the name from the request
+	return &hellopb.HelloResponse{Message: fmt.Sprintf("Hello, %s!", req.GetName())}, nil
+}
+
+// HelloServerStream is a server streaming RPC
+func (s *helloServer) HelloServerStream(req *hellopb.HelloRequest, stream hellopb.GreetingService_HelloServerStreamServer) error {
+	resCount := 5
+	for i := 0; i < resCount; i++ {
+		// send a message to the client
+		if err := stream.Send(&hellopb.HelloResponse{
+			Message: fmt.Sprintf("[%d] Hello, %s!", i, req.GetName()),
+		}); err != nil {
+			return err
+		}
+		time.Sleep(time.Second * 1)
+	}
+	// return nil to indicate that the stream has finished
+	return nil
+}
+
+// HelloClientStream is a client streaming RPC
+func (s *helloServer) HelloClientStream(stream hellopb.GreetingService_HelloClientStreamServer) error {
+	nameList := make([]string, 0)
+	for {
+		// get request from the client
+		req, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			// finish reading the client stream
+			message := fmt.Sprintf("Hello, %v!", nameList)
+			return stream.SendAndClose(&hellopb.HelloResponse{Message: message})
+		}
+		if err != nil {
+			return err
+		}
+		nameList = append(nameList, req.GetName())
+	}
+}
+
+// HelloBiStreams is a bidirectional streaming RPC
+func (s *helloServer) HelloBiStreams(stream hellopb.GreetingService_HelloBiStreamsServer) error {
+	for {
+		// recieve request from the client
+		req, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		// send a message to the client
+		if err := stream.Send(&hellopb.HelloResponse{
+			Message: fmt.Sprintf("Hello, %s!", req.GetName()),
+		}); err != nil {
+			return err
+		}
+	}
+}
--- micro-service/user-service/server.go
+++ micro-service/user-service/server.go
@@ -1,28 +1,16 @@
 package main
 
 import (
-	"context"
-	"errors"
 	"fmt"
-	"io"
+	hellopb "github.com/YukiOnishi1129/auto-stock-trading-system/user-service/grpc"
+	"github.com/YukiOnishi1129/auto-stock-trading-system/user-service/presentational"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
 	"os"
 	"os/signal"
-	"time"
-
-	hellopb "github.com/YukiOnishi1129/auto-stock-trading-system/user-service/grpc"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 )
-
-type myServer struct {
-	hellopb.UnimplementedGreetingServiceServer
-}
-
-func NewMyServer() *myServer {
-	return &myServer{}
-}
 
 func main() {
 	// crate a listener on TCP port 3001
@@ -37,7 +25,7 @@ func main() {
 	s := grpc.NewServer()
 
 	// register the greeting service with the gRPC server
-	hellopb.RegisterGreetingServiceServer(s, NewMyServer())
+	hellopb.RegisterGreetingServiceServer(s, presentational.NewHelloServer())
 
 	// register reflection service on gRPC server
 	reflection.Register(s)
@@ -54,64 +42,4 @@ func main() {
 	<-quit
 	log.Println("Shutting down gRPC server...")
 	s.GracefulStop()
-}
-
-// Hello is a simple unary RPC
-func (s *myServer) Hello(ctx context.Context, req *hellopb.HelloRequest) (*hellopb.HelloResponse, error) {
-	// get the name from the request
-	return &hellopb.HelloResponse{Message: fmt.Sprintf("Hello, %s!", req.GetName())}, nil
-}
-
-// HelloServerStream is a server streaming RPC
-func (s *myServer) HelloServerStream(req *hellopb.HelloRequest, stream hellopb.GreetingService_HelloServerStreamServer) error {
-	resCount := 5;
-	for i := 0; i < resCount; i++ {
-		// send a message to the client
-		if err := stream.Send(&hellopb.HelloResponse{
-			Message: fmt.Sprintf("[%d] Hello, %s!", i, req.GetName()),
-		}); err != nil {
-			return err
-		}
-		time.Sleep(time.Second * 1)
-	}
-	// return nil to indicate that the stream has finished
-	return nil
-}
-
-// HelloClientStream is a client streaming RPC
-func (s *myServer) HelloClientStream(stream hellopb.GreetingService_HelloClientStreamServer) error {
-	nameList := make([]string, 0)
-	for {
-		// get request from the client
-		req, err := stream.Recv()
-		if errors.Is(err, io.EOF) {
-			// finish reading the client stream
-			message := fmt.Sprintf("Hello, %v!", nameList)
-			return stream.SendAndClose(&hellopb.HelloResponse{Message: message})
-		}
-		if err != nil {
-			return err
-		}
-		nameList = append(nameList, req.GetName())
-	}
-}
-
-// HelloBiStreams is a bidirectional streaming RPC
-func (s *myServer) HelloBiStreams(stream hellopb.GreetingService_HelloBiStreamsServer) error {
-	for {
-		// recieve request from the client
-		req, err := stream.Recv()
-		if errors.Is(err, io.EOF) {
-			return nil
-		}
-		if err != nil {
-			return err
-		}
-		// send a message to the client
-		if err := stream.Send(&hellopb.HelloResponse{
-			Message: fmt.Sprintf("Hello, %s!", req.GetName()),
-		}); err != nil {
-			return err
-		}
-	}
 }
