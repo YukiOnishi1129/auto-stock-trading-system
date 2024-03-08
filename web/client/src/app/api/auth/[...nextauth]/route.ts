ec16a7fa764cc46d6cd1622e26de5724490dc9b8
yukionishi1129 <yuki.onishi1129@gmail.com>
2024-03-09 02:16:18 +0900

    prisma user table

--- web/client/src/app/api/auth/[...nextauth]/route.ts
+++ web/client/src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,17 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 // import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+// import { prisma } from "@/lib/prisma";
+
+const prisma = new PrismaClient();
 
 export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
+  // @ts-ignore
+  adapter: PrismaAdapter(prisma),
   providers: [
     // GithubProvider({
     //   clientId: process.env.GITHUB_CLIENT_ID as string,
