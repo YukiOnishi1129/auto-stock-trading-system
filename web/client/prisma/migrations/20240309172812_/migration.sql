4a1b112a663fa0e9f351115c6c443a0ae4fcbc8e
yukionishi1129 <yuki.onishi1129@gmail.com>
2024-03-10 02:34:28 +0900

    fix prisma migration file

---
+++ web/client/prisma/migrations/20240309172812_/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - Added the required column `updated_at` to the `accounts` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `sessions` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `users` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `verification_tokens` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `accounts` ADD COLUMN `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updated_at` DATETIME(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `sessions` ADD COLUMN `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updated_at` DATETIME(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `users` ADD COLUMN `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `deleted_at` DATETIME(3) NULL,
+    ADD COLUMN `updated_at` DATETIME(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `verification_tokens` ADD COLUMN `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updated_at` DATETIME(3) NOT NULL;
