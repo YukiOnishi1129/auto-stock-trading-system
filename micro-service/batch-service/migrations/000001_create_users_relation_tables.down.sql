8b2883e20a57a02092f95d8becfc7e18b4f15e03
yukionishi1129 <yuki.onishi1129@gmail.com>
2024-03-10 02:48:42 +0900

    fix rollback

--- micro-service/batch-service/migrations/000001_create_users_relation_tables.down.sql
+++ micro-service/batch-service/migrations/000001_create_users_relation_tables.down.sql
@@ -1,3 +1,7 @@
+ALTER TABLE accounts DROP FOREIGN KEY accounts_userId_fkey;
+
+ALTER TABLE sessions DROP FOREIGN KEY sessions_userId_fkey;
+
 DROP TABLE IF EXISTS users;
 
 DROP TABLE IF EXISTS accounts;
@@ -5,3 +9,4 @@ DROP TABLE IF EXISTS accounts;
 DROP TABLE IF EXISTS sessions;
 
 DROP TABLE IF EXISTS verification_tokens;
+
